2e2abeeeae1763544cb92dda2bbe285e5a9e377b
scaryclam <git@scaryclam.co.uk>
2011-07-21 12:35:03 +0100

    updated with setup.py to allow installation. Fixed a typo

---
+++ pynmea/utils.py
@@ -0,0 +1,2 @@
+""" Functions that get used by multiple classes go in here
+"""
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="pynmea",
+      version="0.2",
+      description="Python NMEA Library",
+      author="Becky Lewis",
+      author_email="pynmea@scaryclam.co.uk",
+      url="https://code.google.com/p/pynmea/",
+      packages=['pynmea'])
